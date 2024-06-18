9b1e0a5e308a1bcf83e32c62658e5b48313f9d8c
Crowdbotics <engineering@crowdbotics.com>
2024-06-18 16:06:22 +0000

    Initial commit

---
+++ backend/tohdh_dev_135924/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for tohdh_dev_135924 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "tohdh_dev_135924.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tohdh-dev-135924",
+  url: "https://tohdh-dev-135924.botics.co",
+  api: "https://tohdh-dev-135924.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
